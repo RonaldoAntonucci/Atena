bd5f23514eaf23881a23b2e152c34c2ee342e62c
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-09-09 19:16:45 -0300

    Create Questions integration tests

--- server/__tests__/questions/CreateQuestions.e2e-spec.ts
+++ server/__tests__/questions/CreateQuestions.e2e-spec.ts
@@ -1,16 +1,20 @@
 import request from 'supertest';
+import { Repository, getRepository } from 'typeorm';
 
 import Server from 'shared/infra/http/server';
 import { QuestionsRouter, FakeQuestion } from 'modules/questions';
+import Question from '@modules/questions/infra/typeorm/entities/Question';
 import Database from '../util/Database';
 
 describe('Create Questions - e2e', () => {
   let app: Server;
   let db: Database;
+  let questionsRepo: Repository<Question>;
 
   beforeAll(async () => {
     db = new Database();
     await db.start();
+    questionsRepo = getRepository('Question');
 
     app = new Server({ routes: QuestionsRouter });
   });
@@ -30,6 +34,19 @@ describe('Create Questions - e2e', () => {
     expect(newQuestion).toHaveProperty('updatedAt');
     expect(newQuestion).toHaveProperty('title', questionAttrs.title);
     expect(newQuestion).toHaveProperty('text', questionAttrs.text);
+
+    const persistedQuestion = await questionsRepo.findOne(newQuestion.id);
+    expect(persistedQuestion).toHaveProperty('id', newQuestion.id);
+    expect(persistedQuestion).toHaveProperty(
+      'createdAt',
+      new Date(newQuestion.createdAt),
+    );
+    expect(persistedQuestion).toHaveProperty(
+      'updatedAt',
+      new Date(newQuestion.updatedAt),
+    );
+    expect(persistedQuestion).toHaveProperty('title', newQuestion.title);
+    expect(persistedQuestion).toHaveProperty('text', newQuestion.text);
   });
 
   afterAll(async () => {
